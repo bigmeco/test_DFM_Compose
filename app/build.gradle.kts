999471c051b4e806d171161dcf69931d3d2763fb
bigmeco <bigmecoprog@gmail.com>
2023-12-06 00:32:43 +0500

    clearing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,11 +62,4 @@ dependencies {
     implementation("com.google.android.play:core-ktx:1.8.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
